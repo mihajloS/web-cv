e3772dea2d95ba8535c489f0eefddad927595e11
Mihajlo Supic <mihajlosupic@Mihajlos-MacBook-Air.local>
2019-01-10 12:50:21 +0100

    Add basic routing and generate few empty componenets

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { InitStageComponent } from './init-stage/init-stage.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: InitStageComponent},
+  {path: "about", component: AboutComponent},
+  {path: "contact", component: ContactComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { InitStageComponent } from './init-stage/init-stage.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationComponent,
-    InitStageComponent
+    InitStageComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
